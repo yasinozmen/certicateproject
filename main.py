82c870e67534e06ff41cf8c5a022b30f0df15feb
yasinozmen <81865185+yasinozmen@users.noreply.github.com>
2022-06-29 01:18:54 +0300

    write nama on certificate

---
+++ main.py
@@ -0,0 +1,11 @@
+from PIL import Image, ImageDraw, ImageFont
+img = Image.open('cert_empty.png')
+d = ImageDraw.Draw(img)
+fnt=ImageFont.truetype(r"C:\Users\Pc\Downloads\arial.ttf", 90)
+
+text = "Yasin Özmen"
+x=img.width / 2
+y=img.height / 2
+
+d.text((x+60, y-120), text, font=fnt, fill=(0, 0, 0),)
+img.save(text + '.png')
