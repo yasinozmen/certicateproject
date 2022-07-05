f1908e8d35abcd407e05a97e8bf8cedb9e99f5c3
sinanartun <sinanartun@gmail.com>
2022-07-05 18:17:37 +0200

    hello africa

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ img = Image.open('cert_empty.png')
 d = ImageDraw.Draw(img)
 fnt=ImageFont.truetype(r"C:\Users\Pc\Downloads\arial.ttf", 90)
 
-text = "Sinan Artun"
+text = "Sinan Artun2"
 x=img.width / 2
 y=img.height / 2
 
